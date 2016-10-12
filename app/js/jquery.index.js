4f9c41d13fc2428f53d4d9450f5d4b1c5b07d4ed
Scorpionsc <petruninnikolay@gmail.com>
2016-10-12 12:15:59 +0300

    site popup

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,42 +1,49 @@
-"use strict";
-( function() {
-
-    $( function() {
-
-        new Preloader();
-
-    } );
+var preloader = null;
+(function () {
+    'use strict';
 
     var Preloader = function () {
 
         //private properties
         var _self = this,
-            _window = $( window ),
-            _body = $('body');
+            _window = window,
+            _body = document.body;
 
         //private methods
         var _addEvents = function () {
 
-                _window.on( {
-                    load: function(){
+                _window.onload = function() {
 
-                        _showSite();
+                    _showSite();
 
-                    }
-                } );
+                };
 
             },
             _init = function () {
 
-                _body[0].preloader = _self;
                 _addEvents();
+
+            },
+            _addClass = function (elements, className) {
+
+                for (var i = 0; i < elements.length; i++) {
+                    var element = elements[i];
+                    if (element.classList) {
+                        element.classList.add(className);
+                    } else {
+                        element.className += ' ' + className;
+                    }
+                }
 
             },
             _showSite = function() {
 
                 setTimeout(function(){
-                    $('.site').addClass( 'site_loaded' );
+
+                    _addClass(  document.getElementsByClassName('site'), 'site_loaded' );
+
                 },500);
+
             };
 
         //public properties
@@ -47,4 +54,9 @@
         _init();
     };
 
-} )();
+
+
+    preloader = new Preloader();
+
+
+})();
